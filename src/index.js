1e8b9519b305b38f36e74a2ab6e9c88e18b4f4d5
AakanshaMangla <52341767+AakanshaMangla@users.noreply.github.com>
2020-06-13 15:29:01 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div><h1>List of my Favourite Food Itms</h1>
+                <ul>
+                  <li>Chilly Potato</li>
+                  <li>Cake</li>
+                  <li>Suiji Idli</li>
+                </ul></div>, document.getElementById("root"));
